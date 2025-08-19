6627a7c5bd9bb55ba6242e02fa6daec384ecf82c
Amgademadd <amgad2108571@miuegypt.edu.eg>
2025-08-19 04:04:29 +0300

    akher haga amlnaha fe el drag box

--- src/components/LayoutEditor.jsx
+++ src/components/LayoutEditor.jsx
@@ -2,28 +2,34 @@ import React from "react";
 import { Rnd } from "react-rnd";
 
 export default function LayoutEditor({ box, setBox, text }) {
+  const { x, y, width, height } = box;
+
   return (
     <div
       style={{
-        width: "280px",
-        height: "280px",
+        width: "400px", // The container's width
+        height: "400px", // The container's height
         position: "relative",
         border: "2px dashed gray",
         background: "white",
       }}
     >
       <Rnd
-        size={{ width: box.width, height: box.height }}
-        position={{ x: box.x, y: box.y }}
+        // The size is now fixed based on the 'box' prop
+        size={{ width: width, height: height }}
+        position={{ x: x, y: y }}
+        
+        // This is the important change: we only allow dragging
         onDragStop={(e, d) => setBox({ ...box, x: d.x, y: d.y })}
-        onResizeStop={(e, direction, ref, delta, position) => {
-          setBox({
-            width: parseInt(ref.style.width),
-            height: parseInt(ref.style.height),
-            ...position,
-          });
-        }}
+        
+        // We remove the onResizeStop handler to disable resizing
+        // onResizeStop={(e, direction, ref, delta, position) => { ... }}
+        
         bounds="parent"
+        
+        // This style property explicitly hides the resize handles
+        resizeHandleWrapperStyle={{ display: 'none' }}
+
         style={{
           border: "1px solid red",
           background: "rgba(255,0,0,0.1)",
